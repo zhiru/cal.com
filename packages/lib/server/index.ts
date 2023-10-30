4064901fcf0697675fa038a365a2aeb3f3bf4ce5
Keith Williams <keithwillcode@gmail.com>
2023-10-30 11:42:13 -0300

    Remove import

--- packages/lib/server/index.ts
+++ packages/lib/server/index.ts
@@ -4,6 +4,5 @@ export { checkDurationLimits, checkDurationLimit } from "./checkDurationLimits";
 export { defaultHandler } from "./defaultHandler";
 export { defaultResponder } from "./defaultResponder";
 export { getLuckyUser } from "./getLuckyUser";
-export { getServerErrorFromUnknown } from "./getServerErrorFromUnknown";
 export { getTranslation } from "./i18n";
 export { default as perfObserver } from "./perfObserver";

7adde72a28dad70ec6feed6ede1fb2ceef4985c1
Joe Au-Yeung <j.auyeung419@gmail.com>
2024-06-14 16:52:06 -0400

    WIP

--- apps/api/v1/test/lib/users/_get.test.ts
+++ apps/api/v1/test/lib/users/_get.test.ts
@@ -1,5 +1,6 @@
-import prismaMock from "../../../../../../../tests/libs/__mocks__/prismaMock";
+import prismaMock from "../../../../../../tests/libs/__mocks__/prisma";
 
+import type { Request, Response } from "express";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createMocks } from "node-mocks-http";
 import { describe, test } from "vitest";
@@ -9,6 +10,10 @@ import handler from "../../../pages/api/users/_get";
 type CustomNextApiRequest = NextApiRequest & Request;
 type CustomNextApiResponse = NextApiResponse & Response;
 
+// vi.mock("~/lib/helpers/withMiddleware", () => {
+//   return;
+// });
+
 describe("GET /api/users", () => {
   describe("organization scope", () => {
     test("Can get users in an organization", async () => {
@@ -17,13 +22,17 @@ describe("GET /api/users", () => {
         body: {},
       });
       await prismaMock.team.create({
-        teamId: 1,
-        isOrganization: false,
+        data: {
+          id: 1,
+          name: "Non-org team",
+          isOrganization: false,
+        },
       });
       req.isSystemWideAdmin = true;
+      req.userId = 123;
 
-      await handler(req, res);
-      return;
+      const response = await handler(req, res);
+      console.log("🚀 ~ test ~ response:", response);
     });
   });
 });

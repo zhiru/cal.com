ef2b1ea5eef5fb8859cfd469ca49cd5cb77010c2
zomars <zomars@me.com>
2024-06-24 12:42:44 +0200

    Update vercel-staging-deploy.sh

--- apps/web/scripts/vercel-staging-deploy.sh
+++ apps/web/scripts/vercel-staging-deploy.sh
@@ -3,9 +3,20 @@
 if [ "$SKIP_APP_DIR" == "1" ]; then
   echo "Skipping app directory build"
   rm -rf \
-    app/\
-    components/PageWrapperAppDir.tsx\
-    lib/app-providers-app-dir.tsx\
-    .next/types/app/
+    app/future/[user]\
+    app/future/auth\
+    app/future/booking\
+    app/future/connect-and-join\
+    app/future/d\
+    app/future/enterprise\
+    app/future/insights\
+    app/future/maintenance\
+    app/future/more\
+    app/future/org\
+    app/future/payment\
+    app/future/reschedule\
+    app/future/routing-forms\
+    app/future/signup\
+    app/future/team
 fi
 if [ "$VERCEL_ENV" == "preview" ]; then exit 1; else exit 0; fi

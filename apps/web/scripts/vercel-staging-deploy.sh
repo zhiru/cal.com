6fa2e6d62bf6b9ebd3d0a900a3f8c59d5c55b997
zomars <zomars@me.com>
2024-06-24 13:32:58 +0200

    Update vercel-staging-deploy.sh

--- apps/web/scripts/vercel-staging-deploy.sh
+++ apps/web/scripts/vercel-staging-deploy.sh
@@ -3,6 +3,19 @@
 if [ "$SKIP_APP_DIR" == "1" ]; then
   echo "Skipping app directory build"
   rm -rf \
+    app/future/event-types\
+    app/future/apps/installed\
+    app/future/apps/\[slug\]\
+    app/future/apps/\[slug\]/setup\
+    app/future/apps/categories\
+    app/future/apps/categories/\[category\]\
+    app/future/workflows\
+    app/future/settings/teams\
+    app/future/getting-started\
+    app/future/apps\
+    app/future/bookings\
+    app/future/video\
+    app/future/teams\
     app/future/[user]\
     app/future/auth\
     app/future/booking\

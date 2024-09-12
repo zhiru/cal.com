fa376a1aa78442e75403789a68cb7f4969b2b3fd
sean <sean@brydon.io>
2024-09-12 15:02:08 +0100

    wip auth

--- packages/lib/getSafeRedirectUrl.ts
+++ packages/lib/getSafeRedirectUrl.ts
@@ -1,4 +1,4 @@
-import { CONSOLE_URL, WEBAPP_URL, WEBSITE_URL } from "@calcom/lib/constants";
+import { CONSOLE_URL, WEBAPP_URL, WEBSITE_URL, CAL_VIDEO_V2_URL } from "@calcom/lib/constants";
 
 // It ensures that redirection URL safe where it is accepted through a query params or other means where user can change it.
 export const getSafeRedirectUrl = (url = "") => {
@@ -14,7 +14,11 @@ export const getSafeRedirectUrl = (url = "") => {
   const urlParsed = new URL(url);
 
   // Avoid open redirection security vulnerability
-  if (![CONSOLE_URL, WEBAPP_URL, WEBSITE_URL].some((u) => new URL(u).origin === urlParsed.origin)) {
+  if (
+    ![CONSOLE_URL, WEBAPP_URL, WEBSITE_URL, CAL_VIDEO_V2_URL].some(
+      (u) => new URL(u).origin === urlParsed.origin
+    )
+  ) {
     url = `${WEBAPP_URL}/`;
   }
 
